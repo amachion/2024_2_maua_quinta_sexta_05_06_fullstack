26e73fc4bb706f6f77887419a3f1db415063c75d
Andreia <andreia.machion@saojudas.br>
2024-10-11 12:56:04 -0300

    frontend com tabela

--- front/js/script.js
+++ front/js/script.js
@@ -5,5 +5,18 @@ const filmesEndpoint = '/filmes'
 async function obterFilmes() {
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data
-    console.log(filmes)
+
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    for (let filme of filmes) {
+        //para cada filme, inserir uma linha
+        let linha = corpoTabela.insertRow(0)
+        //para cada linha, insere 2 células
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        //atribui o html para cada célula
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+    //console.log(filmes)
 }

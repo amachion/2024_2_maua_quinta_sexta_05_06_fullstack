8d280ae25f49e27ec6b8cb5d33c6a45e0a1d60df
Andreia <andreia.machion@saojudas.br>
2024-10-17 09:45:52 -0300

    cadastro de filmes sem validação

--- front/js/script.js
+++ front/js/script.js
@@ -5,7 +5,6 @@ const filmesEndpoint = '/filmes'
 async function obterFilmes() {
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data
-
     let tabela = document.querySelector('.filmes')
     let corpoTabela = tabela.getElementsByTagName('tbody')[0]
     for (let filme of filmes) {
@@ -18,5 +17,24 @@ async function obterFilmes() {
         celulaTitulo.innerHTML = filme.titulo
         celulaSinopse.innerHTML = filme.sinopse
     }
-    //console.log(filmes)
+}
+async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value 
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    const filmes = (await axios.post (URLcompleta, {titulo, sinopse})).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
 }

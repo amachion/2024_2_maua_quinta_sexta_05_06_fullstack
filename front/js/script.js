53d8f95e11bf71da903a48d516c9e38dc1759a8a
Andreia <andreia.machion@saojudas.br>
2024-10-18 12:57:48 -0300

    usuário cadastrado com validação

--- front/js/script.js
+++ front/js/script.js
@@ -1,32 +1,7 @@
 const protocolo = 'http://'
 const baseURL = 'localhost:3000'
 const filmesEndpoint = '/filmes'
-
-async function obterFilmes() {
-    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
-    const filmes = (await axios.get(URLcompleta)).data
-    let tabela = document.querySelector('.filmes')
-    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
-    for (let filme of filmes) {
-        //para cada filme, inserir uma linha
-        let linha = corpoTabela.insertRow(0)
-        //para cada linha, insere 2 células
-        let celulaTitulo = linha.insertCell(0)
-        let celulaSinopse = linha.insertCell(1)
-        //atribui o html para cada célula
-        celulaTitulo.innerHTML = filme.titulo
-        celulaSinopse.innerHTML = filme.sinopse
-    }
-}
-async function cadastrarFilme() {
-    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
-    let tituloInput = document.querySelector('#tituloInput')
-    let sinopseInput = document.querySelector('#sinopseInput')
-    let titulo = tituloInput.value 
-    let sinopse = sinopseInput.value
-    tituloInput.value = ""
-    sinopseInput.value = ""
-    const filmes = (await axios.post (URLcompleta, {titulo, sinopse})).data
+function exibirFilmes (filmes) {
     let tabela = document.querySelector('.filmes')
     let corpoTabela = tabela.getElementsByTagName('tbody')[0]
     corpoTabela.innerHTML = ""
@@ -36,5 +11,32 @@ async function cadastrarFilme() {
         let celulaSinopse = linha.insertCell(1)
         celulaTitulo.innerHTML = filme.titulo
         celulaSinopse.innerHTML = filme.sinopse
+    }
+}
+async function obterFilmes() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    const filmes = (await axios.get(URLcompleta)).data
+    exibirFilmes(filmes)
+}
+async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value 
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    if (titulo && sinopse) {
+        const filmes = (await axios.post (URLcompleta, {titulo, sinopse})).data
+        exibirFilmes(filmes)
+    }
+    else {
+        let alert = document.querySelector('.alert')
+        alert.classList.add('show')
+        alert.classList.remove('d-none')
+        setTimeout(() => {
+            alert.classList.remove('show')
+            alert.classList.add('d-none')
+        }, 2000)
     }
 }
